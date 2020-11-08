c6a0850e7b4cbd4d191fb9e91b9d87dce91bc85e
Thuong Nguyen <mttbit@gmail.com>
2020-11-08 13:30:58 +0700

    encodeNumber

--- LargestPrimeFactor.java
+++ LargestPrimeFactor.java
@@ -25,8 +25,12 @@ public class LargestPrimeFactor {
   }
 
   static boolean isPrime(int n) {
+    if (n <= 1) {
+      return false;
+    }
+
     int sqrt = (int) Math.sqrt(n);
-    for (int i = 2; i < sqrt; i++) {
+    for (int i = 2; i <= sqrt; i++) {
       if (n % i == 0) {
         return false;
       }
