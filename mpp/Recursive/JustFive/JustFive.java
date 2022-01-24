2d193213d90cee20353fc9f7b245bcc8ba5b14cb
Thuong Nguyen <mttbit@gmail.com>
2022-01-23 23:09:08 -0600

    JustFive

---
+++ mpp/Recursive/JustFive/JustFive.java
@@ -0,0 +1,14 @@
+package mpp.Recursive.JustFive;
+
+public class JustFive {
+  // public static JustFive getJustFive() {
+  //   if (justFiveInstances == null) {
+  //   justFiveInstances = new ArrayList<>();
+  //   }
+  //   if (justFiveInstances.size() < 5)
+  //   justFileInstance.add(new JustFive());
+  //   return justFiveInstance.get(size - 1);
+  //   }
+  //   throw new IndexOutOfBoundsException("Out of size");
+  // }
+}
