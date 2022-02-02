0b674ca140af3f04be979e7395989b5bc6faa6bd
Thuong Nguyen <mttbit@gmail.com>
2022-02-02 14:36:51 -0600

    MyArrayList

---
+++ mpp/ArrayList/MyArrayList/MyArrayList.java
@@ -0,0 +1,84 @@
+package mpp.ArrayList.MyArrayList;
+
+public class MyArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyArrayList array = new MyArrayList();
+		System.out.println(array.toString());
+		array.add("A");
+		array.add("B");
+		System.out.println(array.toString());
+		array.add("C");
+		System.out.println(array.toString());
+		String removeValue = array.remove(1);
+		System.out.println("Removed value: " + removeValue);
+		System.out.println(array.toString());
+	}
+
+	// DO NOT MODIFIED THIS LINE OF CODE
+	private String[] arr = new String[8];
+
+	public int size = 0;
+
+	public void add(String value) {
+		// Do not allow null value to be added in the list
+		if (value == null)
+			return;
+
+		if (size >= arr.length) {
+			// Resize the array
+			resizeArray(1);
+		}
+
+		arr[size] = value;
+		size++;
+	}
+
+	private void resizeArray(int length) {
+		String[] newArray = new String[arr.length + length];
+		for (int i = 0; i < arr.length; i++) {
+			newArray[i] = arr[i];
+		}
+		arr = newArray;
+	}
+
+	@Override
+	public String toString() {
+		String result = "[";
+		for (int i = 0; i < arr.length; i++) {
+			String value = arr[i];
+			if (value == null)
+				continue;
+			
+			if (i != 0) {
+				result += ",";
+			}
+			result += arr[i];
+		}
+		result += "]";
+		return result;
+	}
+	
+	public String remove(int k) {
+		if (k > size || k < 0) {
+			return null;
+		}
+		String kValue = arr[k];
+		String[] temp = new String[size-1];
+		for (int i = 0; i < size - 1; i++) {
+			if (i < k) {
+				temp[i] = arr[i];
+			} else {
+				temp[i] = arr[i+1];
+			}
+		}
+		// Update array
+		arr = temp;
+		
+		// Update size
+		size--;
+		
+		return kValue;
+	}
+}
